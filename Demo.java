05c49d0cf47ad283e4f380f714602c42537f8465
rjk19 <reesejk34@gmail.com>
2021-10-14 04:42:35 -0700

    nieuwe gemaakte opdrachten

--- Demo.java
+++ Demo.java
@@ -1,6 +1,51 @@
+import java.util.Arrays;
 
 public class Demo {
-	public static void main(String[] args) {
-		System.out.println("hello");
+	
+	   public static void main(String[] args) {
+		    
+		   Auto a = new Auto();
+		   Monteur m = new Monteur();
+		   m.repareren(a);
+		  	   
+	   }
+	   
+}
+
+class Auto {
+	
+	boolean motor;
+	boolean deur;
+	boolean voorruit;
+	boolean uitlaat;
+
+	boolean o[];
+	Auto() {
+		
+		   o = new boolean[4];
+		   o[0] = false;
+		   o[1] = true;
+		   o[2] = false;
+		   o[3] = false;
+		   
+		   System.out.println(Arrays.toString(o));
+		
+	}
+	  
+}
+
+class Monteur {
+	int prijs = 0;
+	void repareren(Auto auto) {
+		
+		for(int i = 0; i < auto.o.length; i++) {
+			if(auto.o[i] == true) {
+				prijs += 25;
+				auto.o[i] = false;
+			}
+		}
+		
+		System.out.println(prijs);
+		
 	}
 }
